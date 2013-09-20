b07b3fc7aa9df465b724bc9f04aaf86a8ad56fa0
Jie Feng <flyfengjie@gmail.com>
2013-09-20 01:18:58 -0400

    owh performs worse, need to debug

--- Hashing/OWH/demoOWH.m
+++ Hashing/OWH/demoOWH.m
@@ -14,38 +14,38 @@
 % some parameters
 Ntraining = 3000; % number training samples
 Ntest = 3000; % number test samples
-averageNumberNeighbors = 50; % number of groundtruth neighbors on training set (average)
+averageNumberNeighbors = 1000; % number of groundtruth neighbors on training set (average)
 aspectratio = 0.5; % aspect ratio between the two axis
 loopbits = [2 4 8 16 32]; % try different number of bits for coding
 
 % uniform distribution with larger range
-data_range = 100;
-Xtraining = rand([Ntraining,2]) * data_range; 
-Xtraining(:,2) = aspectratio * Xtraining(:,2);
-Xtest = rand([Ntest,2]) * data_range; 
-Xtest(:,2) = aspectratio * Xtest(:,2);
+% random high dimension data
+feat_dim = 30;
+Xtraining = rand([Ntraining, feat_dim]) * data_range; 
+%Xtraining(:,2) = aspectratio * Xtraining(:,2);
+Xtest = rand([Ntest, feat_dim]) * data_range; 
+%Xtest(:,2) = aspectratio * Xtest(:,2);
 
 % define ground-truth neighbors (this is only used for the evaluation):
 DtrueTraining = distMat(Xtraining);
 DtrueTestTraining = distMat(Xtest,Xtraining); % size = [Ntest x Ntraining]
-Dball = sort(DtrueTraining,2);
-Dball = mean(Dball(:,averageNumberNeighbors));  % mean distance for neighbors in training set
-WtrueTestTraining = DtrueTestTraining < Dball;  % neighbors for testing sampels in training set
-TrainingNeighbors = DtrueTraining < Dball;
+[Dball gt_sorted_idx] = sort(DtrueTraining, 2);
+%Dball = mean(Dball(:,averageNumberNeighbors));  % mean distance for neighbors in training set
+%WtrueTestTraining = DtrueTestTraining < Dball;  % neighbors for testing sampels in training set
+%TrainingNeighbors = DtrueTraining < Dball;
 
 train_pairs = cell(size(TrainingNeighbors,1), 2);
 for i=1:size(TrainingNeighbors,1)
     
-    train_pairs{i,1} = find(TrainingNeighbors(i,:) == 1);
-    train_pairs{i,2} = find(TrainingNeighbors(i,:) == 0);
+    train_pairs{i,1} = gt_sorted_idx(i, 1:averageNumberNeighbors);
+    train_pairs{i,2} = gt_sorted_idx(i, averageNumberNeighbors:end);
     
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % prepare parameters
 
-feat_dim = 2;
-bit_num = 16;
+bit_num = 32;
 
 OWHParams.nbits = bit_num;
 OWHParams.prev_weights = ones(1, OWHParams.nbits) / OWHParams.nbits;
@@ -89,7 +89,7 @@ for i=1:3000
     
 end
 
-%disp(old_params.cur_weights);
+disp(OWHParams.cur_weights);
 
 % evaluation
 % check best neighbors with weighted hamming distance and ground truth
@@ -98,18 +98,17 @@ end
 % compute lsh hamming distance
 lsh_dist = weightedHam(train_codes, train_codes, ones(1, bit_num));
 [lsh_sorted_dist, lsh_sorted_idx] = sort(lsh_dist, 2);
-
-lsh_inter = intersect(lsh_sorted_idx(1, find(lsh_sorted_dist(i, :)==0)), train_pairs{1,1});
+lsh_inter = intersect( lsh_sorted_idx(1, 1:averageNumberNeighbors), train_pairs{1,1} );
 
 % compute weighted hamming distance
 owh_dist = weightedHam(train_codes, train_codes, OWHParams.cur_weights);
 [owh_sorted_dist, owh_sorted_idx] = sort(owh_dist, 2);
+owh_inter = intersect( owh_sorted_idx(1, 1:averageNumberNeighbors), train_pairs{1,1} );
 
 % compute precision-recall with ground truth
 % precision
 for i=1:size(owh_sorted_idx, 1)
     
-    owh_inter = intersect(owh_sorted_idx(1, find(owh_sorted_dist(i, :)==0)), train_pairs{1,1});
     disp(size(inter,1) / size(train_pairs{i,1},1));
     
 end
--- Hashing/OWH/weightLearner.m
+++ Hashing/OWH/weightLearner.m
@@ -5,10 +5,17 @@ function owh_params = weightLearner( owh_params, triplet )
 code_diff_pos = (triplet.query_code - triplet.pos_code).^2;
 code_diff_neg = (triplet.query_code - triplet.neg_code).^2;
 
+hinge_loss = ( double(code_diff_pos - code_diff_neg) ).^2 * owh_params.cur_weights' + 0.5;
+if hinge_loss < 0
+    return;
+end
+
 grad = (owh_params.cur_weights - owh_params.prev_weights) + owh_params.lamda * (double(code_diff_pos - code_diff_neg));
 
 owh_params.prev_weights = owh_params.cur_weights;
 owh_params.cur_weights = owh_params.cur_weights .* exp(-owh_params.eta * grad);
+
+owh_params.cur_weights = owh_params.cur_weights ./ sum(owh_params.cur_weights);
 
 if(sum(isnan(owh_params.cur_weights)) > 0)
     disp('Error');
